c798496cee2791bd1086cc97b9948f2021ecdf1c
Akarshit Baru <akarshitbaru1@gmail.com>
2022-02-25 18:12:06 +0530

    Created Board and Square components

---
+++ src/components/Square.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/function-component-definition */
+/* eslint-disable react/self-closing-comp */
+import React from 'react';
+
+const Square = ({value}) => {
+  return (
+    <button type="button">{value}</button>
+  );
+}
+
+export default Square;
+
