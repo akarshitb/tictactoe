14182fa1b69445263451852d28bbed173f9ac632
Akarshit Baru <akarshitbaru1@gmail.com>
2022-03-01 20:07:39 +0530

    Added Styles

--- src/components/MessageStatus.jsx
+++ src/components/MessageStatus.jsx
@@ -2,17 +2,21 @@ import React from 'react'
 
 const MessageStatus = ({winner,current}) => {
 
-  // const message = winner
-  //   ? `Winner is ${winner}`
-  //   : `Next Player is ${current.isXNext ? 'X' : 'O'}`;
   const noMovesLeft= current.board.every(el=>
     el !==null);
   return (
-    <h2>
-     { winner && `Winner is ${winner}` }
-     { !winner && !noMovesLeft && `Next Player is ${current.isXNext ? 'X' : 'O'}`}
-     { !winner && noMovesLeft && 'DRAW' }
-    </h2>
+    <div className="status-message">
+     { winner && (<> Winner is <span className={winner === 'X' ? 'text-green' : 'text-orange'}>
+       {winner}</span> </>) }
+     { !winner && !noMovesLeft && (<>
+     Next Player is <span className={current.isXNext ? 'text-green' : 'text-orange'}>
+       {current.isXNext ? 'X' : 'O'}</span>
+     </>)}
+     { !winner && noMovesLeft && 
+     <>
+     <span className="text-green">X</span> and <span className="text-orange">O</span> tied
+     </> }
+    </div >
   )
 }
 
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 
 const Square = ({value,onClick,isWinningSquares}) => {
   return (
-    <button type="button" className='square' onClick={onClick}
+    <button type="button" onClick={onClick}
+
+    className={`square ${isWinningSquares ? 'winning': ''}
+    ${value === 'X'? 'text-green': 'text-orange'}`}
     style={{
      fontWeight:isWinningSquares?'bold':'normal'
     }}>
